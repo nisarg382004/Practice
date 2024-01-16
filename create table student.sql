2ab0ec51256ed561c801ab21399f558ae894d783
Nisarg Kumawat <141359722+nisarg382004@users.noreply.github.com>
2024-01-16 19:44:50 +0530

    Add files via upload

---
+++ create table student.sql
@@ -0,0 +1,9 @@
+ create table student 
+ ( Roll_no int primary key not null,
+  F_name varchar(20),
+  L_name varchar(20),
+  Phone_no varchar
+ );
+ insert into student values(1,'priti','pal','6734256090');
+ insert into student values(2,'pooja','yadav','7864892412');
+ insert into student values(3,'sunny','gupta','6345123435');
