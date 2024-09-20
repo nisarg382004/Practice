2a366b2463bb7c62ab9f4e7c09cab3878ddc85d6
Nisarg Kumawat <141359722+nisarg382004@users.noreply.github.com>
2024-09-20 20:23:38 +0530

    Add files via upload

---
+++ PracticalPython.py
@@ -0,0 +1,47 @@
+#randint()
+import random
+print(random.randint(1,100))
+
+# randrange
+# import random
+# print(random.randrange(1,10))
+# print(random.randrange(1,10,2))
+# print(random.randrange(1,101,10))
+
+# choice
+# import random
+# print(random.choice('Welcome to All'))
+# print(random.choice([12,23,45,67,65,43]))
+# print(random.choice([]))
+
+# shuffle()
+# import random
+# numbers=[12,8,45,67,15,43]
+# print(random.shuffle(numbers))
+
+# sample()
+# import random
+# print(random.sample([1,2,3,4,5,6,7,8,9],4))
+
+# uniform()
+# import random
+# print(random.uniform(1,6))
+
+# seed()
+# import random
+# print(random.seed(2))
+
+# datetime.today()
+# import datetime
+# today=datetime.date.today()
+# print(today)
+
+# date()
+# import datetime
+# datetime.date(2019,11,7)
+# print(datetime.date(2019,11,7))
+
+# time()
+# import datetime
+# x=datetime.time()
+# print(x)
